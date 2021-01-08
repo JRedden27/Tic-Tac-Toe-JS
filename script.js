ebc51fcab09bd072dac3d7d8d364309a017edc3e
John Redden <johnleightonredden@gmail.com>
2021-01-07 20:20:05 -0500

    Fixed a bug and added text for a draw

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-
 let playerSymbol = "X";
 
 function playerSwitch(){
-    if(document.getElementById("title").innerText === "Player 1's Turn"){
-        document.getElementById("title").innerText = "Player 2's Turn";
+    if(document.getElementById("title").innerText === "Player X's Turn"){
+        document.getElementById("title").innerText = "Player O's Turn";
         playerSymbol = "O";
-    } else if (document.getElementById("title").innerText === "Player 2's Turn"){
-        document.getElementById("title").innerText = "Player 1's Turn";
+    } else if (document.getElementById("title").innerText === "Player O's Turn"){
+        document.getElementById("title").innerText = "Player X's Turn";
         playerSymbol = "X";
     }
 }
 
 function checkForWinner(){
-    if(document.getElementById("topLeft").innerText === "X" && document.getElementById("topCenter").innerText === "X" && document.getElementById("topRight").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topLeft").innerText === "O" && document.getElementById("topCenter").innerText === "O" && document.getElementById("topRight").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("centerLeft").innerText === "X" && document.getElementById("center").innerText === "X" && document.getElementById("centerRight").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("centerLeft").innerText === "O" && document.getElementById("center").innerText === "O" && document.getElementById("centerRight").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("bottomLeft").innerText === "X" && document.getElementById("bottomCenter").innerText === "X" && document.getElementById("bottomRight").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("bottomLeft").innerText === "O" && document.getElementById("bottomCenter").innerText === "O" && document.getElementById("bottomRight").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("topLeft").innerText === "X" && document.getElementById("centerLeft").innerText === "X" && document.getElementById("bottomLeft").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topLeft").innerText === "O" && document.getElementById("centerLeft").innerText === "O" && document.getElementById("bottomLeft").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("topCenter").innerText === "X" && document.getElementById("center").innerText === "X" && document.getElementById("bottomCenter").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topCenter").innerText === "O" && document.getElementById("center").innerText === "O" && document.getElementById("bottomCenter").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("topRight").innerText === "X" && document.getElementById("centerRight").innerText === "X" && document.getElementById("bottomRight").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topRight").innerText === "O" && document.getElementById("centerRight").innerText === "O" && document.getElementById("bottomRight").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("topLeft").innerText === "X" && document.getElementById("center").innerText === "X" && document.getElementById("bottomRight").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topLeft").innerText === "O" && document.getElementById("center").innerText === "O" && document.getElementById("bottomRight").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
-    } else if(document.getElementById("topRight").innerText === "X" && document.getElementById("center").innerText === "X" && document.getElementById("bottomLeft").innerText === "X"){
-        document.getElementById("title").innerText = "Player 1 Wins!";
-    } else if(document.getElementById("topRight").innerText === "O" && document.getElementById("center").innerText === "O" && document.getElementById("bottomLeft").innerText === "O"){
-        document.getElementById("title").innerText = "Player 2 Wins!";
+    if(document.getElementById("topLeft").innerText === document.getElementById("topCenter").innerText && document.getElementById("topLeft").innerText === document.getElementById("topRight").innerText && document.getElementById("topRight").innerText !== "?"){
+        playerSymbol = document.getElementById("topLeft").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("centerLeft").innerText === document.getElementById("center").innerText && document.getElementById("centerLeft").innerText === document.getElementById("centerRight").innerText && document.getElementById("centerLeft").innerText !== "?"){
+        playerSymbol = document.getElementById("centerLeft").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("bottomLeft").innerText === document.getElementById("bottomCenter").innerText && document.getElementById("bottomLeft").innerText === document.getElementById("bottomRight").innerText && document.getElementById("bottomLeft").innerText !== "?"){
+        playerSymbol = document.getElementById("bottomLeft").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topLeft").innerText === document.getElementById("centerLeft").innerText && document.getElementById("topLeft").innerText === document.getElementById("bottomLeft").innerText && document.getElementById("topLeft").innerText !== "?"){
+        playerSymbol = document.getElementById("topLeft").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topCenter").innerText === document.getElementById("center").innerText && document.getElementById("topCenter").innerText === document.getElementById("bottomCenter").innerText && document.getElementById("topCenter").innerText !== "?"){
+        playerSymbol = document.getElementById("topCenter").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topRight").innerText === document.getElementById("centerRight").innerText && document.getElementById("topRight").innerText === document.getElementById("bottomRight").innerText && document.getElementById("topRight").innerText !== "?"){
+        playerSymbol = document.getElementById("topRight").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topLeft").innerText === document.getElementById("center").innerText && document.getElementById("topLeft").innerText === document.getElementById("bottomRight").innerText && document.getElementById("topLeft").innerText !== "?"){
+        playerSymbol = document.getElementById("topLeft").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topRight").innerText === document.getElementById("center").innerText && document.getElementById("topRight").innerText === document.getElementById("bottomLeft").innerText && document.getElementById("topRight").innerText !== "?"){
+        playerSymbol = document.getElementById("topRight").innerText;
+        document.getElementById("title").innerText = `Player ${playerSymbol} wins!`;
+    } else if(document.getElementById("topLeft").innerText !== "?" && document.getElementById("topCenter").innerText !== "?" && document.getElementById("topRight").innerText !== "?" && document.getElementById("centerLeft").innerText !== "?" && document.getElementById("center").innerText !== "?" && document.getElementById("centerRight").innerText !== "?" && document.getElementById("bottomLeft").innerText !== "?" && document.getElementById("bottomCenter").innerText !== "?" && document.getElementById("bottomRight").innerText !== "?"){
+        document.getElementById("title").innerText = "It's a draw!";
     }
 }
 
 function addSymbol(event){
-    if(event.currentTarget.innerText !== "X" && event.currentTarget.innerText !== "O"){
+    if(event.currentTarget.innerText !== "X" && event.currentTarget.innerText !== "O" && document.getElementById("title").innerText === "Player X's Turn" || document.getElementById("title").innerText === "Player O's Turn"){
         if(playerSymbol === "X"){
             event.currentTarget.innerText = `${playerSymbol}`;
             event.currentTarget.style.color = "blue";
@@ -62,13 +55,12 @@ function addSymbol(event){
 };
 
 document.querySelectorAll(".grid-item").forEach(item => {
-    item.addEventListener("click", addSymbol)
+        item.addEventListener("click", addSymbol)
 });
 
 function clearBoard(){
     playerSymbol = "X";
-    winner = false;
-    document.getElementById("title").innerText = "Player 1's Turn";
+    document.getElementById("title").innerText = "Player X's Turn";
     document.getElementById("topLeft").style.color = "white";
     document.getElementById("topLeft").innerText = "?";
     document.getElementById("topCenter").style.color = "white";
